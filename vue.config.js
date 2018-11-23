914f3f26f4ce3f9cbd3bffd73f22e286e5d8742e
Bell Eapen <github@gulfdoctor.net>
2018-11-23 09:40:26 -0500

    env added to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // options...
-    baseUrl: process.env.NODE_ENV === 'production' ? '/openmrs/owa/phr/' : '/',
+    baseUrl: process.env.NODE_ENV === 'production' ? '/openmrs/owa/drishti/' : '/',
 
     // Other options include:
     // outputDir: undefined,
@@ -9,4 +9,13 @@ module.exports = {
     // productionSourceMap: undefined,
     // parallel: undefined,
     // css: undefined,
+
+    // drishti
+    googleFitShim : process.env.NODE_ENV === 'production' ? 'googlefit' : 'googlefit',
+    fitbitShim: process.env.NODE_ENV === 'production' ? 'fitbit' : 'fitbit',
+    omhOnFhirClientId: process.env.NODE_ENV === 'production' ? '93651a15-4664-486e-8661-eca7ebc21bda' : '93651a15-4664-486e-8661-eca7ebc21bda',
+    omhOnFhirScope: process.env.NODE_ENV === 'production' ? 'patient/*.read launch' : 'patient/*.read launch',
+    omhOnFhirRedirectUri: process.env.NODE_ENV === 'production' ? 'http://mohawk.canehealth.com/openmrs/owa/drishti/login' : '/login',
+    omhOnFhirAPIBase: process.env.NODE_ENV === 'production' ? 'http://tomcat.nuchange.ca' : 'http://tomcat.nuchange.ca',
+    omhOnFhirAPIShimmerAuth: process.env.NODE_ENV === 'production' ? '/shimmerAuthentication' : '/shimmerAuthentication',
 };
